d897b12a8b6576a67672a9d9a20dbb783cb66a14
Adrian Petre <adrian.petre@endava.com>
2023-10-19 01:00:33 +0300

    ConversionUtils.java

--- src/main/java/ro/go/adrhc/util/ConversionUtils.java
+++ src/main/java/ro/go/adrhc/util/ConversionUtils.java
@@ -1,11 +1,25 @@
 package ro.go.adrhc.util;
 
+import com.rainerhahnekamp.sneakythrow.functional.SneakyFunction;
+import lombok.experimental.UtilityClass;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+@UtilityClass
 public class ConversionUtils {
+	public static <T, R, E extends Exception> List<R> convertAll(
+			SneakyFunction<T, Optional<R>, E> converter, Collection<T> items) throws E {
+		List<R> result = new ArrayList<>();
+		for (T t : items) {
+			converter.apply(t).ifPresent(result::add);
+		}
+		return result;
+	}
+
 	public static <T, R> List<R> convertAll(Function<T, Optional<R>> converter, Collection<T> items) {
 		return items.stream()
 				.map(converter)
