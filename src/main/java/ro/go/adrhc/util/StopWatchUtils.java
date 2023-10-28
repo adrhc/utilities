b10bb7078bea092ea565c87fe789dfeb6f226ed7
Adrian <adrhc@outlook.com>
2023-10-28 23:25:43 +0300

    StopWatchUtils.java

---
+++ src/main/java/ro/go/adrhc/util/StopWatchUtils.java
@@ -0,0 +1,13 @@
+package ro.go.adrhc.util;
+
+import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.time.StopWatch;
+
+@UtilityClass
+public class StopWatchUtils {
+	public static StopWatch start() {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		return stopWatch;
+	}
+}
