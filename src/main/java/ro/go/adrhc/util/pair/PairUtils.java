53f128f97c5130f618d7ef9c24f51b03d84f9cee
Adrian <adrhc@outlook.com>
2023-11-07 18:52:23 +0200

    PairUtils.toPairsCollection

---
+++ src/main/java/ro/go/adrhc/util/pair/PairUtils.java
@@ -0,0 +1,12 @@
+package ro.go.adrhc.util.pair;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.stream.Stream;
+
+@UtilityClass
+public class PairUtils {
+	public static <K, V> PairsCollection<K, V> toPairsCollection(Stream<Pair<K, V>> pairs) {
+		return pairs.collect(PairsCollection::empty, PairsCollection::add, PairsCollection::addAll);
+	}
+}
