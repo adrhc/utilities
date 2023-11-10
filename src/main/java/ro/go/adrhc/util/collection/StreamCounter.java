83894b79d83a17afeeb507e574d09270b0c01d63
Adrian <adrhc@outlook.com>
2023-11-10 15:40:18 +0200

    StreamCounter.java

---
+++ src/main/java/ro/go/adrhc/util/collection/StreamCounter.java
@@ -0,0 +1,28 @@
+package ro.go.adrhc.util.collection;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+public class StreamCounter {
+	private final StreamCountHolder count = new StreamCountHolder();
+
+	public <T> Stream<T> countedStream(Stream<T> stream) {
+		return stream.peek(count::increment);
+	}
+
+	public long getCount() {
+		return count.getCount();
+	}
+
+	@Getter
+	private static class StreamCountHolder {
+		private long count;
+
+		public void increment(Object o) {
+			count++;
+		}
+	}
+}
