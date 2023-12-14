53cab6c377b224d387c142d625983b40950c1c60
Adrian <adrhc@outlook.com>
2023-12-14 21:33:14 +0200

    ConsumerUtils.java

---
+++ src/main/java/ro/go/adrhc/util/fn/ConsumerUtils.java
@@ -0,0 +1,10 @@
+package ro.go.adrhc.util.fn;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ConsumerUtils {
+    public static void doNothing(Object object) {
+        // do nothing
+    }
+}
