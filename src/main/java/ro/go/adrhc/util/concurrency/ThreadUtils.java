6a357a390276f035e651076e36259dc16dea5318
Adrian <adrhc@outlook.com>
2024-05-10 22:29:04 +0300

    ThreadUtils.safelySleep(Duration duration)

--- src/main/java/ro/go/adrhc/util/concurrency/ThreadUtils.java
+++ src/main/java/ro/go/adrhc/util/concurrency/ThreadUtils.java
@@ -3,12 +3,22 @@ package ro.go.adrhc.util.concurrency;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
+
 @UtilityClass
 @Slf4j
 public class ThreadUtils {
     public static void safelySleep(long millis) {
         try {
             Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public static void safelySleep(Duration duration) {
+        try {
+            Thread.sleep(duration);
         } catch (InterruptedException e) {
             log.error(e.getMessage(), e);
         }
