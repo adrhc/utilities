ed3ff0771e45240739f9de44c591c16b157793ca
Adrian <adrhc@outlook.com>
2024-05-24 09:48:17 +0300

    ComparisonUtils.java

--- src/main/java/ro/go/adrhc/util/ComparisonUtils.java
+++ src/main/java/ro/go/adrhc/util/ComparisonUtils.java
@@ -1,5 +1,8 @@
 package ro.go.adrhc.util;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class ComparisonUtils {
 	public static <T> int compareComparable(Comparable<T> comparable, T other) {
 		if (comparable == null && other == null) {
@@ -11,5 +14,34 @@ public class ComparisonUtils {
 		} else {
 			return comparable.compareTo(other);
 		}
+	}
+
+	public static <T extends Comparable<T>> int compareComparable(T comparable, T other) {
+		if (comparable == null && other == null) {
+			return 0;
+		} else if (comparable == null) {
+			return -1;
+		} else if (other == null) {
+			return 1;
+		} else {
+			return comparable.compareTo(other);
+		}
+	}
+
+	public static <T extends Comparable<T>> int compareLists(List<T> list, List<T> other) {
+		if (list.size() > other.size()) {
+			return 1;
+		} else if (list.size() < other.size()) {
+			return -1;
+		} else if (new HashSet<>(list).containsAll(other)) {
+			return 0;
+		}
+		for (int i = 0; i < list.size(); i++) {
+			int comparison = compareComparable(list.get(i), other.get(i));
+			if (comparison != 0) {
+				return comparison;
+			}
+		}
+		return 0;
 	}
 }
