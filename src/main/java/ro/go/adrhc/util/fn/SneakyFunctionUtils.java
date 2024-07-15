eab3ddf834c3372090d64046d63aa3f9a021b594
Adrian <adrhc@outlook.com>
2024-07-15 21:58:30 +0300

    SneakyFunctionUtils.toNullResultFn

--- src/main/java/ro/go/adrhc/util/fn/SneakyFunctionUtils.java
+++ src/main/java/ro/go/adrhc/util/fn/SneakyFunctionUtils.java
@@ -6,37 +6,31 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 @UtilityClass
 @Slf4j
 public class SneakyFunctionUtils {
 	public static <T, R, E extends Exception>
-	SneakyFunction<T, Optional<R>, E> toOptionalResult(Function<T, R> fn) {
-		return t -> Optional.ofNullable(fn.apply(t));
-	}
-
-	public static <T, R, E extends Exception> Function<T, R>
-	toFunction(R fallBackValue, SneakyFunction<T, R, E> sneakyfn) {
+	Function<T, Optional<R>> toOptionalResultFn(SneakyFunction<T, R, E> fn) {
 		return t -> {
 			try {
-				return sneakyfn.apply(t);
+				return Optional.ofNullable(fn.apply(t));
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
 			}
-			return fallBackValue;
+			return Optional.empty();
 		};
 	}
 
 	public static <T, R, E extends Exception> Function<T, R>
-	toFunction(Supplier<R> fallBackSupplier, SneakyFunction<T, R, E> sneakyfn) {
+	toNullResultFn(SneakyFunction<T, R, E> sneakyfn) {
 		return t -> {
 			try {
 				return sneakyfn.apply(t);
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
 			}
-			return fallBackSupplier.get();
+			return null;
 		};
 	}
 }
