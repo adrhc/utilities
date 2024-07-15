d25cd1dbe678aa2b501df3fa7e0df1c609e86b26
Adrian <adrhc@outlook.com>
2024-07-15 21:36:03 +0300

    MutableBoolean.java removed OptionalStream.java

--- src/main/java/ro/go/adrhc/util/optional/OptionalStream.java
+++
@@ -1,8 +0,0 @@
-package ro.go.adrhc.util.optional;
-
-import ro.go.adrhc.util.ObjectHolder;
-
-import java.util.stream.Stream;
-
-public interface OptionalStream<T> extends Stream<T>, ObjectHolder<Stream<T>>, OptionalStatus {
-}
