ff0538c16be4df8dae876c718fca70fd16a63662
Adrian <adrhc@outlook.com>
2024-07-16 15:44:32 +0300

    Broken<T>

--- src/main/java/ro/go/adrhc/util/specialcase/Broken.java
+++ src/main/java/ro/go/adrhc/util/specialcase/Broken.java
@@ -1,10 +1,26 @@
 package ro.go.adrhc.util.specialcase;
 
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 @FunctionalInterface
-public interface Broken {
+public interface Broken<T> {
 	boolean isBroken();
 
 	default boolean isOk() {
 		return !isBroken();
+	}
+
+	default <R> Optional<R> mapIfOk(Function<? super T, R> mapper) {
+		return isBroken() ? Optional.empty() : Optional.ofNullable(mapper.apply((T) this));
+	}
+
+	default Optional<T> toOptional() {
+		return isBroken() ? Optional.empty() : Optional.of((T) this);
+	}
+
+	default Stream<T> toStream() {
+		return isBroken() ? Stream.empty() : Stream.of((T) this);
 	}
 }
