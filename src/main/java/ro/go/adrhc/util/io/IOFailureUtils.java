92e7a7968a4b5112e93ca8ff32038f822346e7f2
Adrian <adrhc@outlook.com>
2024-11-23 00:52:54 +0200

    IOFailureUtils.toSafeConsumer

--- src/main/java/ro/go/adrhc/util/io/IOFailureUtils.java
+++ src/main/java/ro/go/adrhc/util/io/IOFailureUtils.java
@@ -5,11 +5,22 @@ import com.rainerhahnekamp.sneakythrow.functional.SneakyPredicate;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Slf4j
 public class IOFailureUtils {
+	public static <T> Consumer<T> toSafeConsumer(SneakyFunction<T, ?, IOException> function) {
+		return t -> {
+			try {
+				function.apply(t);
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+		};
+	}
+
 	public static <T, R> Function<T, R> failToNull(SneakyFunction<T, R, IOException> function) {
 		return t -> {
 			try {
