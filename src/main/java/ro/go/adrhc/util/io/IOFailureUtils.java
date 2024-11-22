1ead5de28b86d9df5e448de95acb07fb6049e437
Adrian <adrhc@outlook.com>
2024-11-23 00:49:03 +0200

    IOFailureUtils.failToNull

--- src/main/java/ro/go/adrhc/util/io/IOFailureUtils.java
+++ src/main/java/ro/go/adrhc/util/io/IOFailureUtils.java
@@ -1,13 +1,26 @@
 package ro.go.adrhc.util.io;
 
+import com.rainerhahnekamp.sneakythrow.functional.SneakyFunction;
 import com.rainerhahnekamp.sneakythrow.functional.SneakyPredicate;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Slf4j
 public class IOFailureUtils {
+	public static <T, R> Function<T, R> failToNull(SneakyFunction<T, R, IOException> function) {
+		return t -> {
+			try {
+				return function.apply(t);
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+				return null;
+			}
+		};
+	}
+
 	public static <T> Predicate<T> failToFalse(SneakyPredicate<T, IOException> predicate) {
 		return t -> {
 			try {
