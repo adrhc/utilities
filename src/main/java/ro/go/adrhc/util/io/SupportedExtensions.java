2063f6b91bc12d6b40397222676013b41ad26aa9
adrhc <adrhc@outlook.com>
2024-11-30 15:25:38 +0200

    SupportedExtensions

--- src/main/java/ro/go/adrhc/util/io/SupportedExtensions.java
+++ src/main/java/ro/go/adrhc/util/io/SupportedExtensions.java
@@ -1,29 +1,34 @@
 package ro.go.adrhc.util.io;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.nio.file.Path;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.nio.file.Files.isRegularFile;
 import static org.apache.commons.io.FilenameUtils.getExtension;
 import static ro.go.adrhc.util.io.PathUtils.hasFilename;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
 public class SupportedExtensions {
 	private final Set<String> supportedExtensions;
-	private final boolean caseSensitive;
 
-	public static SupportedExtensions ofCaseInsensitive(String... extension) {
-		return new SupportedExtensions(Set.of(extension), false);
+	public static SupportedExtensions of(String... extension) {
+		return of(Set.of(extension));
 	}
 
-	public static SupportedExtensions ofCaseSensitive(String... extension) {
-		return new SupportedExtensions(Set.of(extension), true);
+	public static SupportedExtensions of(Set<String> extensions) {
+		return new SupportedExtensions(extensions.stream()
+				.map(String::toLowerCase).collect(Collectors.toSet()));
 	}
 
+	/**
+	 * Uses case-insensitive file extension comparison!
+	 */
 	public boolean supports(Path path) {
 		return isRegularFile(path) && hasFilename(path) && isSupportedFile(path);
 	}
