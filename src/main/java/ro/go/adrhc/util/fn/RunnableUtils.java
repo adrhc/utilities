b993ad332941d0c0e2d41aadf8d09629705cc2f7
adrhc <adrhc@outlook.com>
2024-12-01 20:16:42 +0200

    RunnableUtils.runSilently

--- src/main/java/ro/go/adrhc/util/fn/RunnableUtils.java
+++ src/main/java/ro/go/adrhc/util/fn/RunnableUtils.java
@@ -1,5 +1,6 @@
 package ro.go.adrhc.util.fn;
 
+import com.rainerhahnekamp.sneakythrow.functional.SneakyBiConsumer;
 import com.rainerhahnekamp.sneakythrow.functional.SneakyConsumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,9 +8,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class RunnableUtils {
-	public static <T, E extends Exception> void run(SneakyConsumer<T, E> sneakyConsumer, T t) {
+	public static <T, E extends Exception> void runSilently(SneakyConsumer<T, E> sneakyConsumer, T t) {
 		try {
 			sneakyConsumer.accept(t);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	public static <T, U, E extends Exception> void runSilently(SneakyBiConsumer<T, U, E> sneakyConsumer, T t, U u) {
+		try {
+			sneakyConsumer.accept(t, u);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
