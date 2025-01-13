e068526c81bdfa1d71a29a70d623611e006d4acd
Adrian <adrhc@outlook.com>
2025-01-13 11:35:58 +0200

    StorageMeasureUnitUtils.KILO_BYTE

--- src/main/java/ro/go/adrhc/util/StorageMeasureUnitUtils.java
+++ src/main/java/ro/go/adrhc/util/StorageMeasureUnitUtils.java
@@ -4,7 +4,9 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class StorageMeasureUnitUtils {
-	public static final long MEGA_BYTE = 1024 * 1024;
+	public static final long KILO_BYTE = 1024;
+	public static final double KILO_BYTE_DOUBLE = 1024d;
+	public static final long MEGA_BYTE = 1024 * KILO_BYTE;
 	public static final double MEGA_BYTE_DOUBLE = 1d * MEGA_BYTE;
 	public static final long GIGA_BYTE = 1024 * MEGA_BYTE;
 	public static final double GIGA_BYTE_DOUBLE = 1d * GIGA_BYTE;
@@ -23,6 +25,10 @@ public class StorageMeasureUnitUtils {
 
 	public static double bytesToMB(long bytes) {
 		return bytes / MEGA_BYTE_DOUBLE;
+	}
+
+	public static double bytesToKB(long bytes) {
+		return bytes / KILO_BYTE_DOUBLE;
 	}
 
 	public static String gbFormatted(long megaBytes) {
