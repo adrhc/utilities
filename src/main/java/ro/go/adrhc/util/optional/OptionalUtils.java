86b219e56fb521f4724fed2751099bd046b9a636
Adrian <adrhc@outlook.com>
2025-04-22 18:41:53 +0300

    OptionalUtils.ifPresent

--- src/main/java/ro/go/adrhc/util/optional/OptionalUtils.java
+++ src/main/java/ro/go/adrhc/util/optional/OptionalUtils.java
@@ -5,11 +5,17 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 @UtilityClass
 @Slf4j
 public class OptionalUtils {
+	public static <T> Optional<T> ifPresent(Consumer<T> consumer, Optional<T> optional) {
+		optional.ifPresent(consumer);
+		return optional;
+	}
+
 	public <T> Optional<T> of(Supplier<? extends T> supplier) {
 		return Optional.of(supplier.get());
 	}
