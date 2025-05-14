e7d5bfc0d0b08ba35aba0244efeac81a451b3e5c
adrhc <adrhc@outlook.com>
2025-05-14 09:47:00 +0300

    ListUtils.emptyIfNull

--- src/main/java/ro/go/adrhc/util/collection/ListUtils.java
+++ src/main/java/ro/go/adrhc/util/collection/ListUtils.java
@@ -2,9 +2,14 @@ package ro.go.adrhc.util.collection;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ListUtils {
 	public static <T> List<T> limit(int maxSize, Collection<T> collection) {
 		return collection.stream().limit(maxSize).toList();
+	}
+
+	public static List<Pattern> emptyIfNull(List<Pattern> list) {
+		return list == null ? List.of() : list;
 	}
 }
