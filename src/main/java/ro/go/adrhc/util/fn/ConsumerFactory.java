061c813141ba1b2205b73f48663142245586d9e3
adrhc <adrhc@outlook.com>
2025-07-27 19:56:10 +0300

    ConsumerFactory.toSilentP2Consumer

--- src/main/java/ro/go/adrhc/util/fn/ConsumerFactory.java
+++ src/main/java/ro/go/adrhc/util/fn/ConsumerFactory.java
@@ -5,12 +5,14 @@ import com.rainerhahnekamp.sneakythrow.functional.SneakyFunction;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @UtilityClass
 @Slf4j
 public class ConsumerFactory {
-	public static <T, E extends Exception> Consumer<T> silence(SneakyFunction<T, ?, E> sneakyFn) {
+	public static <T, E extends Exception> Consumer<T> toSilentConsumer(
+		SneakyFunction<T, ?, E> sneakyFn) {
 		return t -> {
 			try {
 				sneakyFn.apply(t);
@@ -21,7 +23,7 @@ public class ConsumerFactory {
 	}
 
 	public static <P1, P2, E extends Exception> Consumer<P2>
-	toP2Consumer(SneakyBiConsumer<P1, P2, E> sneakyBiConsumer, P1 p1) {
+	toSilentP2Consumer(SneakyBiConsumer<P1, P2, E> sneakyBiConsumer, P1 p1) {
 		return p2 -> {
 			try {
 				sneakyBiConsumer.accept(p1, p2);
@@ -29,5 +31,10 @@ public class ConsumerFactory {
 				log.error(e.getMessage(), e);
 			}
 		};
+	}
+
+	public static <P1, P2> Consumer<P2>
+	toP2Consumer(BiConsumer<P1, P2> biConsumer, P1 p1) {
+		return p2 -> biConsumer.accept(p1, p2);
 	}
 }
