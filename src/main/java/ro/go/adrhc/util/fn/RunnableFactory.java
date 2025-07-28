4cbc972faf89e6f12b09e8c9513ffcb659e7f1c4
adrhc <adrhc@outlook.com>
2025-07-28 16:16:32 +0300

    RunnableFactory.toRunnable

--- src/main/java/ro/go/adrhc/util/fn/RunnableFactory.java
+++ src/main/java/ro/go/adrhc/util/fn/RunnableFactory.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RunnableFactory {
 	public static <T, E extends Exception> Runnable
-	of(SneakyConsumer<T, E> sneakyConsumer, T t) {
+	toRunnable(SneakyConsumer<T, E> sneakyConsumer, T t) {
 		return () -> {
 			try {
 				sneakyConsumer.accept(t);
