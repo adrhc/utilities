d5d08bec17e1871ae92ac0289f2568fcbf2ed857
adrhc <adrhc@outlook.com>
2025-10-19 19:22:37 +0300

    + ExceptionUtils

--- src/main/java/ro/go/adrhc/util/ExceptionUtils.java
+++ src/main/java/ro/go/adrhc/util/ExceptionUtils.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ExceptionUtils {
 	public static <E extends Exception> void
-	rethrowIfClass(Class<E> exceptionClass, Exception exception) throws E {
+	throwIfClass(Class<E> exceptionClass, Exception exception) throws E {
 		if (exceptionClass.isInstance(exception)) {
 			throw (E) exception;
 		}
