279961afa88940870c80b7e0f56ac2b555c5a17a
adrhc <adrhc@outlook.com>
2025-12-29 17:12:03 +0200

    LockWaitTimeoutException

--- src/main/java/ro/go/adrhc/util/concurrency/lock/LockWaitTimeoutException.java
+++ src/main/java/ro/go/adrhc/util/concurrency/lock/LockWaitTimeoutException.java
@@ -1,4 +1,7 @@
 package ro.go.adrhc.util.concurrency.lock;
 
 public class LockWaitTimeoutException extends Exception {
+	public LockWaitTimeoutException(long waitMillis) {
+		super("Failed to acquire lock within " + waitMillis + " ms!");
+	}
 }
--- src/main/java/ro/go/adrhc/util/concurrency/lock/SmartLock.java
+++ src/main/java/ro/go/adrhc/util/concurrency/lock/SmartLock.java
@@ -26,7 +26,7 @@ public class SmartLock extends ReentrantLock {
 				unlock();
 			}
 		} else {
-			throw new LockWaitTimeoutException();
+			throw new LockWaitTimeoutException(waitMillis);
 		}
 	}
 
