d25bce0b9000e595f67a1195b52d3a79d6344c0e
adrhc <adrhc@outlook.com>
2025-12-29 02:07:59 +0200

    BiFunctionFactory.toP1P2BiFn

--- src/main/java/ro/go/adrhc/util/fn/BiFunctionFactory.java
+++ src/main/java/ro/go/adrhc/util/fn/BiFunctionFactory.java
@@ -7,6 +7,11 @@ import java.util.function.BiFunction;
 
 @UtilityClass
 public class BiFunctionFactory {
+	public static <P1, P2, P3, R> BiFunction<P1, P2, R>
+	toP1P2BiFn(TriFunction<P1, P2, P3, R> fn, P3 p3) {
+		return (p1, p2) -> fn.apply(p1, p2, p3);
+	}
+
 	/**
 	 * The first parameter (i.e. tFnOutcome) will be the BiFunction's result.
 	 */
