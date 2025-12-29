f47bd98846c4b02b690acfef55e5475d1dfb2fe7
adrhc <adrhc@outlook.com>
2025-12-29 13:32:53 +0200

    BiConsumerFactory.toP1P2BiConsumer

--- src/main/java/ro/go/adrhc/util/fn/BiConsumerFactory.java
+++ src/main/java/ro/go/adrhc/util/fn/BiConsumerFactory.java
@@ -9,6 +9,11 @@ import java.util.function.Consumer;
 @UtilityClass
 @Slf4j
 public class BiConsumerFactory {
+	public static <P1, P2, P3> BiConsumer<P1, P2>
+	toP1P2BiConsumer(TriConsumer<P1, P2, P3> fn, P3 p3) {
+		return (p1, p2) -> fn.accept(p1, p2, p3);
+	}
+
 	/**
 	 * @return a bi-consumer where the 1st parameter is ignored
 	 */
