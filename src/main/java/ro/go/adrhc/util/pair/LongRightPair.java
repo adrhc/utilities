23ee0a37ad8190d33ea843c82d5d5d5a44ea3259
adrhc <adrhc@outlook.com>
2026-02-01 00:05:37 +0200

    LongRightPair changed back from record to class

--- src/main/java/ro/go/adrhc/util/pair/LongRightPair.java
+++ src/main/java/ro/go/adrhc/util/pair/LongRightPair.java
@@ -1,7 +1,15 @@
 package ro.go.adrhc.util.pair;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Getter
 @Accessors(fluent = true)
-public record LongRightPair<L>(L left, long right) {
+public class LongRightPair<L> {
+	protected final L left;
+	protected final long right;
 }
