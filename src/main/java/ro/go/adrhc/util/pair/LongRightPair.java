cb12825ec21054d5ca578e08b1a9eb06001871cb
adrhc <adrhc@outlook.com>
2026-01-31 23:51:38 +0200

    LongRightPair format

--- src/main/java/ro/go/adrhc/util/pair/LongRightPair.java
+++ src/main/java/ro/go/adrhc/util/pair/LongRightPair.java
@@ -1,15 +1,7 @@
 package ro.go.adrhc.util.pair;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
-@RequiredArgsConstructor
-@EqualsAndHashCode
-@Getter
 @Accessors(fluent = true)
-public class LongRightPair<L> {
-	protected final L left;
-	protected final long right;
+public record LongRightPair<L>(L left, long right) {
 }
