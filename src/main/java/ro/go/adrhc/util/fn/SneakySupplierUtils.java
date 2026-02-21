55142bf61854a1a2086049f8a140d88046f0e71f
adrhc <adrhc@outlook.com>
2026-02-22 00:39:12 +0200

    SneakySupplierUtils.failToNull

--- src/main/java/ro/go/adrhc/util/fn/SneakySupplierUtils.java
+++ src/main/java/ro/go/adrhc/util/fn/SneakySupplierUtils.java
@@ -1,14 +1,24 @@
 package ro.go.adrhc.util.fn;
 
 import com.rainerhahnekamp.sneakythrow.functional.SneakySupplier;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@UtilityClass
 @Slf4j
 public class SneakySupplierUtils {
-	public static <T, E extends Exception> List<T>
-	failToEmptyList(SneakySupplier<List<T>, E> sneakySupplier) {
+	public static <T> T failToNull(SneakySupplier<T, ?> supplier) {
+		try {
+			return supplier.get();
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return null;
+	}
+
+	public static <T> List<T> failToEmptyList(SneakySupplier<List<T>, ?> sneakySupplier) {
 		try {
 			return sneakySupplier.get();
 		} catch (Exception e) {
