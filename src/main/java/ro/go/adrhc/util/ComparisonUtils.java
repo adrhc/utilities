992bc0953ba6f02b6c29d1d53bcfac00512d2f37
adrhc <adrhc@outlook.com>
2026-02-28 17:23:39 +0200

    ComparisonUtils.compareCollections

--- src/main/java/ro/go/adrhc/util/ComparisonUtils.java
+++ src/main/java/ro/go/adrhc/util/ComparisonUtils.java
@@ -1,7 +1,8 @@
 package ro.go.adrhc.util;
 
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public class ComparisonUtils {
 	public static <T> int compareComparable(Comparable<T> comparable, T other) {
@@ -28,20 +29,36 @@ public class ComparisonUtils {
 		}
 	}
 
-	public static <T extends Comparable<T>> int compareLists(List<T> list, List<T> other) {
-		return compareLists(list, other, true);
+	/**
+	 * Compares two collections by their size and, if they are
+	 * of the same size, by their elements, ignoring their order.
+	 */
+	public static <T extends Comparable<T>> int
+	compareCollections(Collection<T> set1, Collection<T> set2) {
+		return compareCollections(set1, set2, true);
 	}
 
-	public static <T extends Comparable<T>> int compareLists(List<T> list,
-		List<T> other, boolean equalContentMeansEqualListsNoMatterTheOrder) {
+	public static <T extends Comparable<T>> int compareCollections(
+		Collection<T> set1, Collection<T> set2, boolean ignoreTheOrder) {
+		List<T> list1 = List.copyOf(set1);
+		List<T> list2 = List.copyOf(set2);
+		return compareLists(list1, list2, ignoreTheOrder);
+	}
+
+	public static <T extends Comparable<T>> int
+	compareLists(List<T> list, List<T> other, boolean ignoreTheOrder) {
 		if (list.size() > other.size()) {
 			return 1;
 		} else if (list.size() < other.size()) {
 			return -1;
-		} else if (equalContentMeansEqualListsNoMatterTheOrder
-		           && new HashSet<>(list).containsAll(other)) {
+		} else if (ignoreTheOrder && Set.copyOf(list).containsAll(other)) {
 			return 0;
 		}
+		return compareElemsOnEqualPositions(list, other);
+	}
+
+	public static <T extends Comparable<T>> int
+	compareElemsOnEqualPositions(List<T> list, List<T> other) {
 		for (int i = 0; i < list.size(); i++) {
 			int comparison = compareComparable(list.get(i), other.get(i));
 			if (comparison != 0) {
