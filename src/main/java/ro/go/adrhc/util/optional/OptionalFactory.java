83419fc4d1fe98c2c67cec398f09cef0af8ae782
adrhc <adrhc@outlook.com>
2026-03-03 18:27:44 +0200

    OptionalFactory.java

--- src/main/java/ro/go/adrhc/util/optional/OptionalFactory.java
+++ src/main/java/ro/go/adrhc/util/optional/OptionalFactory.java
@@ -11,14 +11,6 @@ import java.util.function.Supplier;
 @Slf4j
 public class OptionalFactory {
 	/**
-	 * @return an Optional of the "supplier"'s product if the condition is true, otherwise Optional.empty()
-	 */
-	public <T, E extends Exception> Optional<T>
-	ofConditionedSneakySupplier(boolean condition, SneakySupplier<? extends T, E> supplier) throws E {
-		return condition ? Optional.ofNullable(supplier.get()) : Optional.empty();
-	}
-
-	/**
 	 * @return a not empty Optional of the "supplier"'s product
 	 */
 	public <T> Optional<T> ofSupplier(Supplier<? extends T> supplier) {
@@ -30,6 +22,14 @@ public class OptionalFactory {
 	 */
 	public <T> Optional<T> ofNullableSupplier(Supplier<? extends T> supplier) {
 		return Optional.ofNullable(supplier.get());
+	}
+
+	/**
+	 * @return an Optional of the "supplier"'s product if the condition is true, otherwise Optional.empty()
+	 */
+	public <T, E extends Exception> Optional<T>
+	ofConditionedSneakySupplier(boolean condition, SneakySupplier<? extends T, E> supplier) throws E {
+		return condition ? Optional.ofNullable(supplier.get()) : Optional.empty();
 	}
 
 	/**
